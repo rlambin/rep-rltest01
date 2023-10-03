46734d6eb78e44c8c5dd780f52e4c8bb6a28355a
rlambin <99756405+rlambin@users.noreply.github.com>
2023-10-03 11:28:57 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,11 +1,13 @@
-#!/opt/app-root/bin/python3
-from flask import Flask, request, jsonify
+# /opt/app-root/bin/python3
+# from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route("/hello/<name>")
-def hello_world(name):
-    return jsonify({"message": "Hello, {}!".format(name)})
+#@app.route("/hello/<name>")
+#def hello_world(name):
+#    return jsonify({"message": "Hello, {}!".format(name)})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=8080)
+
+print("hello world!")
